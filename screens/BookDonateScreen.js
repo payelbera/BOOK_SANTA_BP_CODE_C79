c67c462bac6002b81e79290acbb1233f12d7c266
payelbera <60781680+payelbera@users.noreply.github.com>
2021-01-17 00:11:37 +0530

    Add files via upload

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, FlatList,TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements'
+import firebase from 'firebase';
+import db from '../config'
+//import MyHeader from /components/MyHeader
+
+export default class BookDonateScreen extends Component{
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  this.requestRef= null
+  }
+
+  getRequestedBooksList =()=>{
+    
+  }
+
+  componentDidMount(){
+    this.getRequestedBooksList()
+  }
+
+  componentWillUnmount(){
+    this.requestRef();
+  }
+
+  keyExtractor = (item, index) => index.toString()
+
+  renderItem = ( {item, i} ) =>{
+    return (
+      
+    )
+  }
+
+  render(){
+    return(
+      <View style={{flex:1}}>
+        
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  subContainer:{
+    flex:1,
+    fontSize: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    width:100,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8
+     }
+  }
+})
